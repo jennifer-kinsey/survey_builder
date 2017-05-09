1546fd793b711961a3ff87ec474cacc60c7bedd6
Jennifer Kinsey and Monique St. Laurent <monique@>
2017-05-09 11:58:18 -0700

    add some functionality to create a survey and add questions

--- app.rb
+++ app.rb
@@ -9,5 +9,30 @@ require("pg")
 require ("pry")
 
 get('/') do
+  @surveys = Survey.all
   erb(:index)
+end
+
+get('/survey_new') do
+  erb(:create_survey)
+end
+
+post('/survey_add') do
+  name = params.fetch("name")
+  @survey = Survey.create({name: name})
+  @questions = @survey.questions
+  erb(:add_questions)
+end
+
+post('/question_add/:id') do
+  @survey = Survey.find(params.fetch('id').to_i)
+  description = params.fetch("description")
+  @question = Question.create({description: description, survey_id: @survey.id})
+  @questions = @survey.questions
+  erb(:add_questions)
+end
+
+get('/survey_edit/:id') do
+  @survey = Survey.find(params.fetch('id').to_i)
+  erb(:modify_survey)
 end
